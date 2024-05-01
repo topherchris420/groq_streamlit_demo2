378ed243fc826d9c4b30f6bc60eefeb40589f3a1
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-01 16:18:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     model_option = st.selectbox(
-        "Choose a model:",
+        "Connect with the perfect AI match:",
         options=list(models.keys()),
         format_func=lambda x: models[x]["name"],
         index=4  # Default to mixtral
