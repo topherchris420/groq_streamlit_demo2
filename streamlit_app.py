48950ebcd6669d33e70ee58a1a508f80ccf3edb9
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-07-21 11:28:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def icon(emoji: str):
     """Shows an emoji as a Notion-style page icon."""
     st.write(f'<span style="font-size: 78px; line-height: 1">{emoji}</span>', unsafe_allow_html=True)
 
-icon("👩🏾‍⚕️")
+icon("🧬")
 st.markdown('<a href="https://groqhealth.streamlit.app/" style="text-decoration:none; color: #ADD8E6;"><h2>DigiDopps™</h2></a>', unsafe_allow_html=True)
 st.subheader("Meet Leonardo Da Vinci 🫀, Powered by Groq 🚀")
 
